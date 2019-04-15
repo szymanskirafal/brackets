1cfc5473e89f96d1756f052e5183ccc429001afc
Rafal Szymanski <r.szymansky@gmail.com>
2019-04-15 23:47:03 +0200

    Flat methods

--- brackets.py
+++ brackets.py
@@ -9,16 +9,18 @@ class BracketsOpenAndCloseTester:
         self.brackets_in_expression = ''
 
     def find_any_bracket_in_expression(self):
-        expression = self.expression
-        brackets_allowed = self.brackets_allowed
-        bracket_found = [expression.find(x) for x in expression if x in brackets_allowed]
+        bracket_found = [
+            self.expression.find(x) for x in self.expression
+            if x in self.brackets_allowed
+        ]
         print('-- brackets are in expression at positions: ', bracket_found)
         return bracket_found
 
     def create_a_string_with_brackets_found_in_expression(self):
-        expression = self.expression
-        brackets_allowed = self.brackets_allowed
-        brackets_extracted = [x for x in expression if x in brackets_allowed]
+        brackets_extracted = [
+            x for x in self.expression
+            if x in self.brackets_allowed
+        ]
         brackets_extracted = ''.join(brackets_extracted)
         print('-- created string with brackets found: ', brackets_extracted)
         self.brackets_in_expression = brackets_extracted
@@ -27,29 +29,32 @@ class BracketsOpenAndCloseTester:
 
     def remove_pair(self, position = None):
         print('-- will try to remove pair which begins at position ', position)
-        brackets_in_expression = self.brackets_in_expression
-        first_part_of_expression = brackets_in_expression[:position]
-        second_part_of_expression = brackets_in_expression[position+2:]
-        brackets_in_expression = first_part_of_expression + second_part_of_expression
-        print('-- brackets in expression: ', brackets_in_expression)
-        self.brackets_in_expression = brackets_in_expression
+        first_part_of_expression = self.brackets_in_expression[:position]
+        second_part_of_expression = self.brackets_in_expression[position+2:]
+        both_parts = first_part_of_expression + second_part_of_expression
+        self.brackets_in_expression = both_parts
+        print('-- brackets in expression: ', self.brackets_in_expression)
         return self.check_pairs()
 
     def check_brackets(self):
         bracket_found_in_expression = self.find_any_bracket_in_expression()
         if bracket_found_in_expression:
-            self.proccess_with_brackets()
+            self.proccess_with_brackets_in_expression()
         else:
             print("There are no brackets but it's ok.")
             return True
 
     def check_brackets_are_balanced(self):
-        expression = self.expression
-        opening_brackets = self.opening_brackets
         closing_brackets = self.closing_brackets
-        opening_brackets = [x for x in expression if x in opening_brackets]
+        opening_brackets = [
+            x for x in self.expression
+            if x in self.opening_brackets
+        ]
+        closing_brackets = [
+            x for x in self.expression
+            if x in self.closing_brackets
+        ]
         number_of_opening_brackets = len(opening_brackets)
-        closing_brackets = [x for x in expression if x in closing_brackets]
         number_of_closing_brackets = len(closing_brackets)
         if number_of_opening_brackets == number_of_closing_brackets:
             return True
@@ -57,12 +62,10 @@ class BracketsOpenAndCloseTester:
             return False
 
     def check_pairs(self):
-        brackets = self.brackets_in_expression
-        pairs = self.pairs
         positions_of_pair_found = []
-        for pair in pairs:
-            p = brackets.find(pair)
-            positions_of_pair_found.append(p)
+        for pair in self.pairs:
+            position = self.brackets_in_expression.find(pair)
+            positions_of_pair_found.append(position)
         position = max(positions_of_pair_found)
         if position != -1:
             print('-- position: ', position)
@@ -71,7 +74,7 @@ class BracketsOpenAndCloseTester:
             print("-- There aren't any pairs.")
             return False
 
-    def proccess_with_brackets(self):
+    def proccess_with_brackets_in_expression(self):
         self.create_a_string_with_brackets_found_in_expression()
         brackets_are_balanced = self.check_brackets_are_balanced()
         if brackets_are_balanced:
